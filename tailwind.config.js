b2a3dc37c6004acc5adfaffd68704d1e9237a556
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-04 15:02:50 +0200

    fix structure of tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 module.exports = {
   content: ["./disk/**/*.{html,js}"],
   theme: {
-    extend: {},
-    animation: {
-      gradient: 'gradient 5s ease infinite'
-    }
-    keyframes: {
-      gradient: {
-        '0%': {backgroundPosition: 0% 50% },
-        '100%': {backgoundPosition: '100% 50%'}
-      }
-    }
+    extend: {
+      animation: {
+        gradient: "gradient 5s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 80%" },
+          "100%": { backgoundPosition: "100% 80%" },
+        },
+      },
+    },
   },
   plugins: [],
 };
