e8df75fafcfd664c1c0ef4a991e3c1b98c6b4497
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-04 13:41:55 +0200

    add tailwind and configure it

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./disk/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
