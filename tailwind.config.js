91df2fbad44325847010758155dec816d2115efe
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-07 17:56:26 +0200

    modify footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,8 @@ module.exports = {
   content: ["./disk/**/*.{html,js}"],
   theme: {
     extend: {
-      animation: {
-        gradient: "gradient 5s ease infinite",
-      },
-      keyframes: {
-        gradient: {
-          "0%": { backgroundPosition: "0% 80%" },
-          "100%": { backgoundPosition: "100% 80%" },
-        },
-      },
-    },
+
   },
   plugins: [],
-};
-
+},
+}
